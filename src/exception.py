fd4255c6abcc82208ca946d63d57b033494b0090
Akashrajasekar <akash.shantha@gmail.com>
2025-06-13 16:41:37 +0400

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys    # To manipulate different parts of the python runtime environment
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,9 +18,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__=="__main__":
-    try:
-        1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+# if __name__=="__main__":
+#     try:
+#         1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
